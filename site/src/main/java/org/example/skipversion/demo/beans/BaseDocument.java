fdd6af232c9759829d7c886ed1fe3dd68ea69e9f
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-09-19 16:23:59 -0400

    Adding example

---
+++ site/src/main/java/org/example/skipversion/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.example.skipversion.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="skipversiondemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
