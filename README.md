b40cf6a0a358cedb73b061acdf41d97e307d55d0
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-09-20 09:25:28 -0400

    Updating docs

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 Demo project presenting how to skip versioning on normal workflow documents
 by adding a mixin type and customizing ```VersionVariantAction``` SCXML action class.
 
+## Use Cases
+
+- When you import content from the external data source (which should be regarded as the "single source of the truth")
+  to Hippo documents periodically, you normally don't want to create revision history for that kind of imported documents.
+  By default, whenever you import and republish the imported documents, CMS will keep making versions, which can cause
+  the database of CMS can easily bloat.
+
 ## Build and Run the Demo
 
 ```bash
@@ -14,5 +21,6 @@ by adding a mixin type and customizing ```VersionVariantAction``` SCXML action c
 - Edit Product document instead and try to publish it. It doesn't create a version
   because the unpublished variant node of Product document is type of ```skipversiondemo:skipversioning``` mixin type and [SkippableVersionVariantAction.java](cms/src/main/java/org/example/skip/version/demo/cms/workflow/action/SkippableVersionVariantAction.java) which is configured at
   ```/hippo:configuration/hippo:modules/scxmlregistry/hippo:moduleconfig/hipposcxml:definitions/documentworkflow/version/@hipposcxml:classname```.
+
 - You can also add a new Product document. Try to edit and publish it, but it still doesn't create a version at all.
 
--- cms/src/main/java/org/example/skip/version/demo/cms/workflow/action/SkippableVersionVariantAction.java
+++ cms/src/main/java/org/example/skip/version/demo/cms/workflow/action/SkippableVersionVariantAction.java
@@ -6,13 +6,30 @@ import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
+import org.hippoecm.repository.api.Document;
 import org.hippoecm.repository.api.WorkflowException;
 import org.onehippo.repository.documentworkflow.action.VersionVariantAction;
 import org.onehippo.repository.documentworkflow.task.VersionVariantTask;
 
+/**
+ * Custom <code>hippo:version</code> SCXML action by extending the default implementation, {@link VersionVariantAction}.
+ * <P>
+ * This custom implementation checks if the target unpublished node is the custom mixin type,
+ * <code>skipversiondemo:skipversioning</code>, and in that case, just skip the invocation not to create a new version
+ * out of it.
+ * </P>
+ * <P>
+ * To apply this custom SCXML action, you should replace <code>/hippo:configuration/hippo:modules/scxmlregistry/hippo:moduleconfig/hipposcxml:definitions/documentworkflow/version/@hipposcxml:classname</code>
+ * property by the FQCN of this class.
+ * </P>
+ */
 @SuppressWarnings("serial")
 public class SkippableVersionVariantAction extends VersionVariantAction {
 
+    /**
+     * Custom mixin node type to indicate if the unpublished variant node should skip versioning during the document workflow
+     * process.
+     */
     private static final String NT_SKIPVERSIONING = "skipversiondemo:skipversioning";
 
     @Override
@@ -23,8 +40,10 @@ public class SkippableVersionVariantAction extends VersionVariantAction {
                 if (getVariant() != null && getVariant().hasNode()) {
                     final Session workflowSession = getWorkflowContext().getInternalWorkflowSession();
                     Node targetNode = getVariant().getNode(workflowSession);
+
                     if (targetNode.isNodeType(NT_SKIPVERSIONING)) {
-                        return null;
+                        // If it should skip versioning, just return a Document without proceeding.
+                        return new Document(targetNode);
                     }
                 }
 
